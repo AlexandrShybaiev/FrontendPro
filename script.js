dc70ea63fd0b19c00222b3ff0a045ab3b5ca90c5
AlexandrShybaiev <alexandertytflash@gmail.com>
2026-01-29 20:48:10 +0200

    added typeof and console.log

---
+++ script.js
@@ -0,0 +1,27 @@
+
+// String
+let name = "Alex";
+// Number
+let age = 25;
+// Boolean
+let isHuman = false;
+// Undefined js
+let cat;
+// Null (сам пишу) в typeof станет object - баг
+let car = null;
+// BigInt
+let bigNumber = 12345678901234567890n;
+//Symbol
+let id = Symbol("id");
+// Object
+let user = { name: "Alex", age: 18 };
+let user2 ={name: "Alex", age: 20}
+
+console.log("name:", typeof(name));
+console.log("age:", typeof(age));
+console.log("isHuman:", typeof(isHuman));
+console.log("cat:", typeof(cat));
+console.log("bigNumber:", typeof(bigNumber));
+console.log("id:", typeof(id));
+console.log("user:", typeof(user));
+

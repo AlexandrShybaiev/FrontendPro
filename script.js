69dee714ccbd38998e05fa34258ee8f9cb456acf
AlexandrShybaiev <alexandertytflash@gmail.com>
2026-01-29 21:42:44 +0200

    added hm2

---
+++ script.js
@@ -0,0 +1,5 @@
+let userName = prompt("Enter your name");
+let userAge = prompt("Enter your age");
+let userHeight = prompt("Enter your height");
+let helloMessage = (`Hello, ${userName}. Your age ${userAge}. Your height is ${userHeight}. `);
+console.log(helloMessage);

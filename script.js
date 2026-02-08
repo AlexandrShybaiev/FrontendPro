c46f44012720ad05c79047a093702a5700825c41
AlexandrShybaiev <alexandertytflash@gmail.com>
2026-02-08 17:33:17 +0200

    added hm5.1

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+let i = 20;
+for(let i =20; i <=30; i+= 0.5){
+  console.log(i);
+}
+
+//
+// let i = 10;
+// for(let i =10; i <=20; i+= 2){
+//   console.log(i);
+// }
+
+// let i = 13;
+// for(let i =13; i >=5; i-= 1){
+//   console.log(i);
+// }
+

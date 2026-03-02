3ac49c2607b5961e42bbc351dca45031fa68f645
AlexandrShybaiev <alexandertytflash@gmail.com>
2026-03-02 11:14:38 +0200

    added hm8.3

---
+++ script.js
@@ -0,0 +1,19 @@
+function askNumber() {
+  let lastValue;
+
+
+  for (let i = 0; i < 10; i++) {
+    let input = prompt("Введіть число більше 100")
+    let number = Number(input);
+    lastValue = number;
+
+    if (number > 100) {
+      break;
+    }
+  }
+
+
+  console.log(lastValue);
+}
+
+askNumber();

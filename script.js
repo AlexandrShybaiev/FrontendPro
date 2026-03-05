05927f9f074cf7a821ffad4653bbed82666c4556
AlexandrShybaiev <alexandertytflash@gmail.com>
2026-03-05 11:57:04 +0200

    added hm10.1

---
+++ script.js
@@ -0,0 +1,41 @@
+let company = {
+  sales: [
+    {name: 'John', salary: 1000},
+    {name: 'Alice', salary: 600}
+  ],
+  development: {
+    web: [
+      {name: 'Peter', salary: 2000},
+      {name: 'Alex', salary: 1800}
+    ],
+    internals: [
+      {name: 'Jack', salary: 1300}
+    ]
+  }
+};
+
+function sumSalaries(department) {
+  let sum = 0;
+
+  if (Array.isArray(department)) {
+    department.forEach(
+      function (employee) {
+        sum += employee.salary;
+      });
+
+  } else {
+
+    Object.values(department).forEach(
+      function (subdep) {
+        sum += sumSalaries(subdep);
+      });
+
+  }
+
+  return sum;
+}
+
+console.log(
+  sumSalaries(company)
+);
+
